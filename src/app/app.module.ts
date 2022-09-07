d802199ffc7267be0e17404fa13e1ee4487a6ee0
Simona Cotin <simona.cotin@gmail.com>
2022-09-07 16:21:17 +0200

    use imgix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, NgOptimizedImage],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
