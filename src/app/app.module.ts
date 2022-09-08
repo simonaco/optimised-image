bac61771f2a4664b8fbbd60678b1db6f6f70e5b9
Simona Cotin <simona.cotin@gmail.com>
2022-09-08 02:22:10 +0200

    remove NgOptimizedImage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgOptimizedImage } from '@angular/common';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgOptimizedImage],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
