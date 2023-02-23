9459c629355d5613161019cb3c251337f321ac60
Simona Cotin <simona.cotin@gmail.com>
2023-02-23 06:42:49 +0100

    update images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,13 @@ import { Component } from '@angular/core';
   imports: [NgFor, NgOptimizedImage],
   template: `
     <article class="post">
-      <h2>Lorem Ipsum</h2>
+      <h2>Angular Image Directive</h2>
       <figure>
         <img
-          ngSrc="/verona_sunset.jpg"
-          alt="Verona skyline"
-          priority
-          width="800"
-          height="400"
+          ngSrc="/johannes-hofmann-PM5a_R83-YQ-unsplash.jpg"
+          alt="Bernina train in Swiss Alps"
         />
-        <figcaption>Sun setting over Verona</figcaption>
+        <figcaption>Bernina train in Switzerland</figcaption>
       </figure>
       <p>What is Lorem Ipsum?</p>
 
@@ -39,24 +36,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   images = [
     {
-      src: '/amsterdam.jpeg',
-      alt: 'Amsterdam bridge',
+      src: '/aleksandr-popov-I0_UBfHbnkI-unsplash.jpg',
+      alt: 'Train station at sunset',
     },
     {
-      src: '/castelsanpietro.jpeg',
-      alt: 'Two ducks looking over Catel San Pietro',
+      src: '/boris-stromar-qoCZmnwmwe0-unsplash.jpg',
+      alt: 'Train signals at sunset',
     },
     {
-      src: '/teatro_romano.jpeg',
-      alt: 'Teatro romano ruins',
+      src: '/dayso-7o4Du7K6wII-unsplash.jpg',
+      alt: 'Train signal',
     },
     {
-      src: '/arena_0.jpeg',
-      alt: 'Arena di Verona with Italian flag projection',
+      src: '/johannes-hofmann-PM5a_R83-YQ-unsplash.jpg',
+      alt: 'Bernina train in Swiss Alps',
     },
     {
-      src: '/dolomites.jpg',
-      alt: 'Dolomiti mountains covered in snow',
+      src: '/roland-losslein-DmDYX_ltI48-unsplash.jpg',
+      alt: 'Train in Scotland',
     },
   ];
 }
