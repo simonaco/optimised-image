d68d547f1670ec86489f340c775e3f9d73ac08d9
Simona Cotin <simona.cotin@gmail.com>
2023-02-23 06:44:36 +0100

    update images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,34 @@ import { Component } from '@angular/core';
       <h2>Lorem Ipsum</h2>
       <figure>
         <img
-          src="https://best-places.imgix.net/verona_sunset.jpg"
-          alt="Verona skyline"
+          src="https://best-places.imgix.net/johannes-hofmann-PM5a_R83-YQ-unsplash.jpg"
+          alt="Bernina train in Swiss Alps"
         />
-        <figcaption>Sun setting over Verona</figcaption>
+        <figcaption>Bernina train in Switzerland</figcaption>
       </figure>
       <p>What is Lorem Ipsum?</p>
 
       <div *ngFor="let image of images">
+        <p>
+          Cras et sem non ex condimentum finibus et id ante. Integer venenatis,
+          risus eget dapibus varius, lectus nibh elementum ligula, at rutrum
+          arcu lacus non nibh. Pellentesque vehicula eros a congue efficitur.
+          Nullam vitae nulla in ligula lobortis dictum non in nunc. In vitae
+          diam non nunc auctor pellentesque ut et risus. Sed vel diam turpis.
+          Integer bibendum suscipit purus, id finibus dolor. Curabitur sed lacus
+          placerat, volutpat sapien et, fringilla est. Etiam at ipsum elementum,
+          vulputate massa quis, fermentum odio.
+        </p>
+        <p>
+          Cras et sem non ex condimentum finibus et id ante. Integer venenatis,
+          risus eget dapibus varius, lectus nibh elementum ligula, at rutrum
+          arcu lacus non nibh. Pellentesque vehicula eros a congue efficitur.
+          Nullam vitae nulla in ligula lobortis dictum non in nunc. In vitae
+          diam non nunc auctor pellentesque ut et risus. Sed vel diam turpis.
+          Integer bibendum suscipit purus, id finibus dolor. Curabitur sed lacus
+          placerat, volutpat sapien et, fringilla est. Etiam at ipsum elementum,
+          vulputate massa quis, fermentum odio.
+        </p>
         <p>
           Cras et sem non ex condimentum finibus et id ante. Integer venenatis,
           risus eget dapibus varius, lectus nibh elementum ligula, at rutrum
@@ -36,24 +56,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   images = [
     {
-      src: 'https://best-places.imgix.net/amsterdam.jpeg',
-      alt: 'Amsterdam bridge',
+      src: 'https://best-places.imgix.net/aleksandr-popov-I0_UBfHbnkI-unsplash.jpg',
+      alt: 'Train station at sunset',
     },
     {
-      src: 'https://best-places.imgix.net/castelsanpietro.jpeg',
-      alt: 'Two ducks looking over Catel San Pietro',
+      src: 'https://best-places.imgix.net/boris-stromar-qoCZmnwmwe0-unsplash.jpg',
+      alt: 'Train signals at sunset',
     },
     {
-      src: 'https://best-places.imgix.net/teatro_romano.jpeg',
-      alt: 'Teatro romano ruins',
+      src: 'https://best-places.imgix.net/dayso-7o4Du7K6wII-unsplash.jpg',
+      alt: 'Train signal',
     },
     {
-      src: 'https://best-places.imgix.net/arena_0.jpeg',
-      alt: 'Arena di Verona with Italian flag projection',
+      src: 'https://best-places.imgix.net/johannes-hofmann-PM5a_R83-YQ-unsplash.jpg',
+      alt: 'Bernina train in Swiss Alps',
     },
     {
-      src: 'https://best-places.imgix.net/dolomites.jpg',
-      alt: 'Dolomiti mountains covered in snow',
+      src: 'https://best-places.imgix.net/roland-losslein-DmDYX_ltI48-unsplash.jpg',
+      alt: 'Train in Scotland',
     },
   ];
 }
